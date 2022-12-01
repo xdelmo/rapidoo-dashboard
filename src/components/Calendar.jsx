f6dd73b11af8c1f0b8e4ebc9f54dfb3810633570
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-12-01 15:28:06 +0100

    UPDATE: new Calendar component

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import OverTitle from "./OverTitle";
+
+function Calendar() {
+  return (
+    <div>
+      <div className="h-full bg-light rounded-md">
+        <div>Calendar</div>
+      </div>
+    </div>
+  );
+}
+
+export default Calendar;
