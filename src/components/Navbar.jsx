093bdb9ffc5cf73e91448350de85b84490e7f641
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-12-06 12:20:11 +0100

    FIX: navbar button margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar(props) {
             <button
               onClick={handleClick}
               // bottone invisibile su desktop
-              className="mx-auto"
+              className={` ${props.isSidebarOpen ? "ml-auto" : "mx-auto"}`}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
